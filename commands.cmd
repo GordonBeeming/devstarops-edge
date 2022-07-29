32863b598bfd6cb6f1262b1c6162cf8e62a115a2
Gordon Beeming <gordon@devstarops.com>
2022-07-29 23:28:04 +0200

    blog path

--- commands.cmd
+++ commands.cmd
@@ -1,8 +1,12 @@
 podman stop -a
 
+podman build -f .\Containerfile -t edge
+
+podman run -p 8102:80 --name edge --replace --rm -d -v nginx.conf:/etc/nginx/nginx.conf -v access.log:/var/log/nginx/access.log -v error.log:/var/log/nginx/error.log -v edge:/var/edge/ localhost/edge:latest
+
 podman run -p 80:80 -p 443:443 --name server --rm -d -v $PWD/nginx.conf:/etc/nginx/nginx.conf -v $PWD/reverse-error.log:/var/log/nginx/reverse-error.log -v $PWD/reverse-access.log:/var/log/nginx/reverse-access.log -v $PWD/access.log:/var/log/nginx/access.log -v $PWD/error.log:/var/log/nginx/error.log -v $PWD/portal/:/var/portal/ localhost/site:latest
 
-sudo podman exec -it 5d6a4d4b568ff211381d867dcc612f1a972865226f58eff7e359e3d3c9551748 /usr/sbin/nginx -s reload
+sudo podman exec -it 0fd30cba2e8d7558af1de0940e76c18f86f5380126ec7f60f519eb9636dc3ece /usr/sbin/nginx -s reload
 
 sudo podman exec -it 87d871c2de8e4d73d89278357c38e39db7feb5ac3a8824350b938edf55ca144e /var/log/nginx | ls
 
