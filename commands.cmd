a3b66ccca9163cf0ebb4fe40c7d7f93044b6ab0e
Gordon Beeming <gordon@devstarops.com>
2022-08-14 22:45:36 +0200

    update edge with redirects

--- commands.cmd
+++ commands.cmd
@@ -2,9 +2,9 @@ podman stop -a
 
 podman build -f .\Containerfile -t edge
 
-podman run -p 8102:80 --name edge --replace --rm -d -v nginx.conf:/etc/nginx/nginx.conf -v access.log:/var/log/nginx/access.log -v error.log:/var/log/nginx/error.log -v edge:/var/edge/ localhost/edge:latest
+podman run -p 8099:443 --name edge --replace --rm -d -v nginx.conf:/etc/nginx/nginx.conf -v access.log:/var/log/nginx/access.log -v error.log:/var/log/nginx/error.log -v edge:/var/edge/ localhost/edge:latest
 
-podman exec -it dc9cccb3184a3b2ce7af5eaffbef290a953ec62ab1d11fdb6196fff76ce274b1 /usr/sbin/nginx -s reload
+podman exec -it 85bbea8e3512515c21f73a467e37fbe58766ec35bfc4c08940f0c2bec4190719 /usr/sbin/nginx -s reload
 
 podman run -p 80:80 -p 443:443 --name server --rm -d -v $PWD/nginx.conf:/etc/nginx/nginx.conf -v $PWD/reverse-error.log:/var/log/nginx/reverse-error.log -v $PWD/reverse-access.log:/var/log/nginx/reverse-access.log -v $PWD/access.log:/var/log/nginx/access.log -v $PWD/error.log:/var/log/nginx/error.log -v $PWD/portal/:/var/portal/ localhost/site:latest
 
